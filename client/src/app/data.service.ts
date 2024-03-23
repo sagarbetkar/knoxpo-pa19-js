6442843eaf338da5ae6e440da05cab01df0d88b6
Sagar Betkar <sagarbetkar1994@gmail.com>
2024-03-23 07:26:17 +0530

    In data.service apis URL update

--- client/src/app/data.service.ts
+++ client/src/app/data.service.ts
@@ -2,37 +2,50 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
   constructor(private http: HttpClient) {}
 
   createUser(data) {
     //return this.http.post('http://localhost:2611/api/v1/create', data);
-    return this.http.post('api/v1/create', data);
+    return this.http.post(
+      'https://knoxpo-pa19-js.onrender.com/api/v1/create',
+      data
+    );
   }
 
   getAllUser() {
     //return this.http.get('http://localhost:2611/api/v1/getAll');
-    return this.http.get('api/v1/getAll');
+    return this.http.get('https://knoxpo-pa19-js.onrender.com/api/v1/getAll');
   }
 
   deleteUser(id) {
-    return this.http.delete(`/api/v1/delete/${id}`);
+    return this.http.delete(
+      `https://knoxpo-pa19-js.onrender.com/api/v1/delete/${id}`
+    );
     //return this.http.delete(`http://localhost:2611/api/v1/delete/${id}`);
   }
 
   getUserById(id) {
-    return this.http.get(`/api/v1/get/${id}`);
+    return this.http.get(
+      `https://knoxpo-pa19-js.onrender.com/api/v1/get/${id}`
+    );
     //return this.http.get(`http://localhost:2611/api/v1/get/${id}`);
   }
 
   updateUser(data, id) {
-    return this.http.put(`/api/v1/update/${id}`, data);
+    return this.http.put(
+      `https://knoxpo-pa19-js.onrender.com/api/v1/update/${id}`,
+      data
+    );
     //return this.http.put(`http://localhost:2611/api/v1/update/${id}`, data);
   }
   postWeather(data) {
-    return this.http.post('/api/v1/weather', data);
+    return this.http.post(
+      'https://knoxpo-pa19-js.onrender.com/api/v1/weather',
+      data
+    );
     //return this.http.post('http://localhost:2611/api/v1/weather', data);
   }
 }
